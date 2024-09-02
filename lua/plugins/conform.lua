b5c5ba5deab6abff9881759753634907cee7a1d4
Vincent Jousse <vincent@jousse.org>
2024-09-02 16:00:23 +0200

    fix: conform.nvim config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -33,7 +33,7 @@ return {
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
-        lsp_fallback = true,
+        lsp_format = "fallback",
         async = false,
         timeout_ms = 1000,
       })
